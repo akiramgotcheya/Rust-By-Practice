78efffdd7561a221560bc387dcbc7570fa640877
Akira M <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-15 00:46:44 +0530

    Create 9.rs

---
+++ Variables/9.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let (x, y);
+    (x,..) = (3, 4);
+    [.., y] = [1, 2];
+    // Fill the blank to make the code work
+    assert_eq!([x,y], [3,2]);
+
+    println!("Success!");
+} 
+
+// answer is [3,2]
