c79620d833e21115d1892cb567b00512d12ac2c6
Akira M <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-15 00:51:17 +0530

    Update 2.rs

--- Variables/2.rs
+++ Variables/2.rs
@@ -1,10 +1,12 @@
 
-fn main() {
-    let (x, y);
-    (x,..) = (3, 4);
-    [.., y] = [1, 2];
-    // Fill the blank to make the code work
-    assert_eq!([x,y], [3,2]);
 
+// Fill the blanks in the code to make it compile
+fn main() {
+    let mut x =  1;
+    x += 2; 
+    
+    assert_eq!(x, 3);
     println!("Success!");
-} 
+}
+ 
+//marked x as mutable
