c55d9e86f086b677a4dd49dbca05db0c08b3c3e1
Akira M <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-15 00:49:57 +0530

    Update 3.rs

--- Variables/3.rs
+++ Variables/3.rs
@@ -1,10 +1,11 @@
 
+// Fix the error below with least amount of modification
 fn main() {
-    let (x, y);
-    (x,..) = (3, 4);
-    [.., y] = [1, 2];
-    // Fill the blank to make the code work
-    assert_eq!([x,y], [3,2]);
-
-    println!("Success!");
-} 
+    let x: i32 = 10;
+    {
+        let y: i32 = 5;
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+    println!("The value of x is {}", x); 
+}
+ // removed y from line 9
