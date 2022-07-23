2a8e87b41a818222643d062496073ff6aab00425
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-23 19:23:36 +0530

    Update 3.rs

--- Flow Control/3.rs
+++ Flow Control/3.rs
@@ -1,7 +1,14 @@
+//Question 3
+
 fn main() {
-    for n in 1..100 {
+    for n in 1..=99 { // modify this line to make the code work
         if n == 100 {
             panic!("NEVER LET THIS RUN")
         }
     }
+
+    println!("Success!");
 } 
+
+// There is no error in the code but since our goal is to print Success, Replacing 100 in Ln4 with number < 100 will do the trick
+// We can modify Ln5 as well but since the question specifically asks us to modify Ln4, this will be the solution.
