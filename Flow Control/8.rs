dd8eefffc96d3b53950daab9aff40bc5b1c0b2d8
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-23 20:07:09 +0530

    Update 8.rs

--- Flow Control/8.rs
+++ Flow Control/8.rs
@@ -1,14 +1,21 @@
+// Question 8
+// Fill in the blanks
+
 fn main() {
     let mut n = 0;
     for i in 0..=100 {
-        if n != 66 {
-            n += 1;
-            continue;
-        }
-
-        break;
+       if n != 66 {
+           n+=1;
+           continue;
+       }
+       
+       break;
     }
 
     assert_eq!(n, 66);
+
     println!("Success!");
 }
+
+// We are skipping iteration by using continue in Ln9 since the value we are looking for is 66.
+// We end the for loop with break statement. 
