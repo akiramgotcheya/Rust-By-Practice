c9a5cd17ae8f9e72e7c61e053631066784243e12
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-23 19:58:12 +0530

    Update 6.rs

--- Flow Control/6.rs
+++ Flow Control/6.rs
@@ -1,9 +1,12 @@
+//Question 6
+// Fill in the blanks to make the last println! work !
+
 fn main() {
     // A counter variable
     let mut n = 1;
 
     // Loop while the condition is true
-    while n < 10 {
+    while n <= 10 {
         if n % 15 == 0 {
             println!("fizzbuzz");
         } else if n % 3 == 0 {
@@ -15,8 +18,11 @@ fn main() {
         }
 
 
-        n += 1;
+        n+=1;
     }
 
-    println!("n reached {}, soloop is over", n);
+    println!("n reached {}, so loop is over",n);
 }
+
+// Ln9 was while condition was missing and added iteration to counter variable in Ln21 for loop to work
+// Also the question is vague. Asks us to make the last println! work. You can do this by adding break in Ln21 which makes program to print last Ln.
