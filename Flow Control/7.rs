edc46861ae928d59349c1e5a610a18cd975592bf
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-23 20:01:38 +0530

    Update 7.rs

--- Flow Control/7.rs
+++ Flow Control/7.rs
@@ -1,12 +1,18 @@
+// Question 7
+// Fill in the blank
+
 fn main() {
     let mut n = 0;
     for i in 0..=100 {
-        if n == 66 {
-            break;
-        }
-        n += 1;
+       if n == 66 {
+           break;
+       }
+       n += 1;
     }
 
     assert_eq!(n, 66);
+
     println!("Success!");
 }
+
+// added break in Ln8 to work
