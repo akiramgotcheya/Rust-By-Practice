345a53751f212ce1df1043524d987cb38b12318c
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-24 19:46:57 +0530

    Update 1.rs

--- 8. Pattern Match/8.1. match, matches! and if let/1.rs
+++ 8. Pattern Match/8.1. match, matches! and if let/1.rs
@@ -1,1 +1,22 @@
+//Question 1
+// Fill the blanks
 
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+
+fn main() {
+    let dire = Direction::South;
+    match dire {
+        Direction::East => println!("East"),
+        Direction::South|Direction::North  => { // Matching South or North here
+            println!("South or North");
+        },
+        Direction::West => println!("West"),
+    };
+}
+
+// Add direction crate to North,South & West in Ln15 & 18
