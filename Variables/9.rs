d0d166d333f5e6cfe5fecebd020f163b581a1678
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-08-12 17:35:09 +0530

    Update 9.rs

--- Variables/9.rs
+++ Variables/9.rs
@@ -1,11 +1,14 @@
+// Question 10
+// Fill the blank to make the code work
+
 fn main() {
     let (x, y);
     (x,..) = (3, 4);
     [.., y] = [1, 2];
-    // Fill the blank to make the code work
+    
     assert_eq!([x,y], [3,2]);
 
     println!("Success!");
 } 
 
-// answer is [3,2]
+// Answer is [3,2] since x is the first & y si the last value of the given tuples. 
