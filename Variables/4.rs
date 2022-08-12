3039d4c27825326edbd91854057f270ed12fd48a
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-08-12 17:26:47 +0530

    Update 4.rs

--- Variables/4.rs
+++ Variables/4.rs
@@ -1,5 +1,6 @@
-
+// Question 4
 // Fix the error with the use of define_x
+
 fn main() {
     let x = define_x();
     println!("{}, world", x); 
@@ -9,3 +10,5 @@ fn define_x() -> String{
     let x = "hello".to_string();
     return x;
 }
+
+// Binding function define_x to variable x since its out of scope
