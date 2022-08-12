65f3c9539440d9a2421ef394b6d94abdd01c2c1c
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-08-12 17:06:04 +0530

    Update 1.rs

--- Variables/1.rs
+++ Variables/1.rs
@@ -1,5 +1,6 @@
 // Question 1
 // Fix the error below with least amount of modification to the code
+
 fn main() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let _y: i32; // Uninitialized but also unused, only a Warning !
