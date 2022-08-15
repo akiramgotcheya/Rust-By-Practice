260616969875b4ce7bc3780f4c0f6b411917daf0
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-08-15 17:49:12 +0530

    Update 4.rs

--- Basic Types/Numbers/4.rs
+++ Basic Types/Numbers/4.rs
@@ -1,8 +1,11 @@
-
+// Question 4
 // Fill the blanks to make it work
+
 fn main() {
     assert_eq!(i8::MAX, 127); 
     assert_eq!(u8::MAX, 255); 
 
     println!("Success!");
 }
+
+// the max value i8 can represent is 127 & u8 is 255
