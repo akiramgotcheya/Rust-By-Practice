06027272f0d556fbff97ff507a7b893ad4385b39
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-08-15 17:52:18 +0530

    Update 5.rs

--- Basic Types/Numbers/5.rs
+++ Basic Types/Numbers/5.rs
@@ -1,10 +1,11 @@
-
+// Question 5
 // Fix errors and panics to make it work
+
 fn main() {
-   let v1 = 200_u8 + 8;
-   let v2 = i8::checked_add(100, 8).unwrap();
+   let v1 = 247_u8 + 8;
+   let v2 = i8::checked_add(119, 8).unwrap();
    println!("{},{}",v1,v2);
 }
 
 
-//sum of u8 should be =<255 and i8 should be =<127
+//sum of u8 should be <=255 and i8 should be <=127
