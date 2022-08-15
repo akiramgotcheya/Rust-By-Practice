b142268d697e41767afddecbc6ff22b56d46d758
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-08-15 18:14:05 +0530

    Update 8.rs

--- Basic Types/Numbers/8.rs
+++ Basic Types/Numbers/8.rs
@@ -1,4 +1,19 @@
+//Question 8
+// Make it work in two distinct ways
+
 fn main() {
     assert!(0.1_f32 +0.2_f32 ==0.3_f32);
     println!("Success!");
 }
+
+// or
+
+
+fn main() {
+    let v = 0.3_f32;
+    assert!(0.1_f32+0.2_f32==v);
+
+    println!("Success!");
+}
+
+
